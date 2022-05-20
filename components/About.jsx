c51b8d12b2919aa2fa4a44d07cc8d6f5af15b687
Shivang-stack <shivang1691401@gmail.com>
2022-05-20 15:06:51 +0530

    intial commit

---
+++ components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#54cfdf]'>About</p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-300'>// I am not your normal developer</p>
+          <p className='py-2 text-gray-300'>
+          I started my programming journey in 9th standard with java, 
+          developed some small project for school compitions. As the President of Computer Club of my school i organized some Coding compitions as well as 
+          quiz compition where we use the quiz app devloped by me.
+          </p>
+          <p className='py-2 text-gray-300'>
+            Fascinated with how intricate programming can be I was quickly drawn
+            to learn more. I started learning javascript and was even more
+            enthused with making websites interactive. I am
+            now spending my time building projects with React JS, MongoDB, Firebase, Postgres ,and
+            learning new technologies.
+          </p>
+          <p className='py-2 text-gray-300 underline cursor-pointer'>Check out some of my latest projects.</p>
+        </div>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-800 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <img className='rounded-xl' src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Project.jsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import animekartImg from '../public/assets/projects/AnimeKart.png';
+import chatappImg from '../public/assets/projects/charlando-web.png'
+import newsappImg from '../public/assets/projects/NewsForYou.png'
+import videochatImg from '../public/assets/projects/videochat.png'
+import weatherAppImg from '../public/assets/projects/weatherapp.png'
+import ProjectItem from './ProjectItem';
+
+const Projects = () => {
+  return (
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#50a4d1]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I've Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='E-commerce website'
+            backgroundImg={animekartImg}
+            projectUrl='/animekart'
+          />
+          <ProjectItem
+            title='Chat App'
+            backgroundImg={chatappImg}
+            projectUrl='/chatapp'
+          />
+          <ProjectItem
+            title='News Website'
+            backgroundImg={newsappImg}
+            projectUrl='/newswebsite'
+          />
+          <ProjectItem
+            title='Video Chat'
+            backgroundImg={videochatImg}
+            projectUrl='/videochat'
+          />
+          <ProjectItem
+            title='Weather App'
+            backgroundImg={weatherAppImg}
+            projectUrl='/weatherapp'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
