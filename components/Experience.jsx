8f3c8a7cb98bcfa9d0be8a6156a5ec624f67f0ab
shivang-stack <shivang1691401@gmail.com>
2023-06-29 16:45:17 +0530

    Company name added

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -7,17 +7,17 @@ const MyProfilePage = () => {
   const experiences = [
     {
         companyLogo: movella,
-        position: 'SDE Intern',
+        position: 'SDE Intern , Movella Inc',
         description: 'Collaborating with the QA Team to conduct thorough testing on testrail.Utilizing data analytics techniques to support the development of the Minimum Viable Product (MVP).Leveraging Datadog, a powerful data analytics platform, to extract valuable insights for enhancing product functionality and performance. Working on the integration of video capabilities to ensure seamless incorporation of this feature into the product.Contributing to the company objective of delivering a high-quality, efficient, and feature-rich solution to customers through diligent testing and analysis.',
       },
     {
         companyLogo: pysphere,
-        position: 'Full Stack Intern',
+        position: 'Full Stack Intern , Pysphere Technologies LLP',
         description: 'Oversee back-end development using Java Springboot to maintain website integrity, security and efficiency.Build databases and table structures following architecture methodology for web applications.',
     },
     {
         companyLogo: yesmentor,
-        position: 'Full Stack Intern',
+        position: 'Full Stack Intern , YesMentor Pvt Ltd',
         description: 'Developed features such as AudioBook, Blogs And Podcast with node js and express js .Developed a nodejs app with postgres for YesMentor users to keep up with there progress on app',
     },
     // Add more experience objects as needed
