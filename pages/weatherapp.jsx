b1bd5236a4684fbd94e04d723ed917684e0b3b86
shivang-stack <shivang1691401@gmail.com>
2023-07-08 13:26:59 +0530

    Apk button Added

--- pages/weatherapp.jsx
+++ pages/weatherapp.jsx
@@ -42,7 +42,13 @@ const weatherapp = () => {
           >
             <button className='px-8 py-2 mt-4 mr-8'>Code</button>
           </a>
-          
+          <a
+            href='https://drive.google.com/file/d/1atUrnHz7qkgPzf_ggkp-drjmW5TZ3hpu/view?usp=drive_link'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Download Apk</button>
+          </a>
 
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-800 rounded-xl py-4'>
